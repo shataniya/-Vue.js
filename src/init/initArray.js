9ca07c3dd24cf2fd9fd9438462b66bdddac770b6
shataniya <47215073+shataniya@users.noreply.github.com>
2020-06-28 00:29:22 +0800

    Add files via upload

--- src/init/initArray.js
+++ src/init/initArray.js
@@ -6,14 +6,13 @@ function initArray(vm){
     vm.ArrayNewProto = []
     Object.getOwnPropertyNames(Array.prototype).forEach(key => {
         if(typeof vm.arrayMethods[key] === "function"){
-            // let vm = this
             Object.defineProperty(vm.ArrayNewProto, key, {
                 enumerable: false, // 默认是不会枚举的，为了保持数组一样对性质
                 configurable: false, // 不可以重新配置
                 get(){
                     return function(){
                         // console.log("我监听到了数组触发"+key+"事件")
-                        let result = vm.arrayMethods[key].apply(vm, arguments)
+                        let result = vm.arrayMethods[key].apply(this, arguments)
                         vm.deps.forEach(dep => {
                             dep.notify()
                         })
